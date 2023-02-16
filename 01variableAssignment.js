e5a425932c4974fd45897ad897b34838721850c5
aishusankpal0511 <aishusankpal0511@gmail.com>
2023-02-16 11:01:44 +0530

    updated assignment 1
    updated assignment 1

---
+++ 01variableAssignment.js
@@ -0,0 +1,18 @@
+console.log("*******Step 1 ********")
+console.log("My Full Name is:Aishwarya sankpal")
+console.log("My Full Name with Middle Name is:Aishwarya Vaibhav Sankpal")
+
+console.log("******Step 2 ***********")
+console.log("myLove:I love JS")
+
+console.log("*******Step 3*******")
+console.log("My First Name:Aishwarya")
+console.log("My Last Name:Sankpal")
+console.log("My Age:28")
+console.log("Address:At post Bambavade District sangli")
+console.log("pincode:416310")
+console.log("Gender:Female")
+console.log("Marital status:isMarried")
+
+console.log("*******I can write output in one line *********")
+console.log("My Full Name is:Aishwarya Vaibhav sankpal,My Middle Name is:Vaibhav,My Address:At post Bambavade district sangli,pincode:416310,My Gender:Female")
