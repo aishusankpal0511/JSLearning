36ec7072f88426d3508fdae6d701e500b6180a0f
aishusankpal0511 <aishusankpal0511@gmail.com>
2023-02-16 15:47:23 +0530

    updated assignment 01
    updated assignment 01

---
+++ variableAssignment01.js
@@ -0,0 +1,22 @@
+console.log("******* 1st.Step********");
+var myFullName;
+console.log(undefined);
+var myFullName = "Aishwarya Sankpal";
+console.log("My Full Name:", myFullName)
+var myFullName ="Aishwarya Vaibhav Sankpal";
+console.log("My Full Name:",myFullName);
+
+
+console.log("******  2nd. Step********");
+var myLove = "I Love Only JS";
+console.log(myLove);
+
+
+console.log("******* 3rd. Step*******");
+var firstName ="Aishwarya";
+var lastName ="Sankpal";
+var age = "28";
+var address = "Bambavade, 410316";
+var gender = "Female";
+var isYouMarried = "yes";
+console.log("First Name:", firstName, "Last Name:", lastName,"Age:", age, "Address:",address," Gender:",gender, "Is You Married:",isYouMarried);
