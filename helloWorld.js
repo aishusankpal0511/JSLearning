28de04d001539907b0238ef0898921c6977f2502
aishusankpal0511 <aishusankpal0511@gmail.com>
2023-02-16 09:59:41 +0530

    updated personal
    updated personal

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,52 @@
-console.log("Aishwarya Sankpal");
+
+console.log(" Aishwarya Sankpal");
+var fullName = "Aishwarya Sankpal";
+console.log(fullName);
+console.log(fullName);
+
+
+var city; // Variable Declration
+console.log(city);
+city="Pune"; // Variable Initialization
+console.log(city);
+city="Surat"; // Variable Update
+console.log(city);
+
+var universityName ="Pune University";
+console.log(universityName);
+0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
