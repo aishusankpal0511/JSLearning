fd66ff5313ad671089cf5508b02ded9d26c602f9
aishusankpal0511 <aishusankpal0511@gmail.com>
2023-02-17 11:17:23 +0530

    updated basic assignment 01
    updated basic assignment 01

---
+++ 01variableBasicAssign.js
@@ -0,0 +1,33 @@
+console.log("******variable data type*******");
+console.log("******number data type *****");
+
+var age = 28;
+console.log("type of age:",age);
+var dataTypes ="number";
+console.log("data type is:",dataTypes);
+
+console.log("******string data type******");
+age = "28";
+console.log(typeof age);
+var dataTypes = "string";
+console.log("data type is:",dataTypes);
+
+console.log("*****Boolean data type******");
+age = true;
+console.log(typeof age);
+var dataType = "boolean";
+console.log("data type is:",dataType);
+
+console.log("****undefined data types*****");
+var graduation;
+console.log("typeOf graduation:",graduation);
+
+console.log("****null data types******");
+var city = null;
+console.log("typeOf city:",city);
+
+
+
+
+
+
