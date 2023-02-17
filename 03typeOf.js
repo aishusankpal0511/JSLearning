484e9f133b86d3afc2d0cfe34bda2d23293e4fb5
aishusankpal0511 <aishusankpal0511@gmail.com>
2023-02-17 13:46:04 +0530

    updated typeOf 03
    updated typeOf 03

---
+++ 03typeOf.js
@@ -0,0 +1,28 @@
+console.log("*****1*****");
+console.log("*****before initialization******");
+var bankName;
+console.log("bank name is:",bankName);
+console.log("******After initialization******");
+var dataTypes ="SBI Bank";
+console.log("data type is:",dataTypes);
+var marks = 90;
+console.log("******2******");
+console.log("data type value:",marks);
+console.log("*****3******");
+console.log("*****before initialization******");
+var isWorking;
+console.log("isworking",isWorking);
+console.log("******After initialization******");
+var isWorking = true;
+console.log("isworking",isWorking);
+console.log("*****3.1*****")
+var typeOf ="isWorking";
+console.log("type of is:",typeOf);
+console.log("*****4******");
+var totalCount = "Hundred and Seven";
+console.log("total count is:",totalCount);
+totalCount = "107";
+console.log("total count is:",totalCount);
+
+
+
