ce266e7c876f467ef4a6759da48ee8488cb9da81
aishusankpal0511 <aishusankpal0511@gmail.com>
2023-02-17 12:32:04 +0530

    updated swapVariable assign02
    updated swapVariable assign02

---
+++ 02swapVariables.js
@@ -0,0 +1,23 @@
+console.log("******1******");
+console.log("****before swap*****");
+var sweety = "sweety";
+var cutie = "cutie";
+console.log(sweety,cutie);
+console.log("*****After swap****");
+temp=cutie;
+cutie=sweety;
+sweety=temp;
+console.log(sweety,cutie);
+console.log("******2*****");
+console.log("****After swap*****");
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+console.log("num1,num2,num3");
+console.log("****before swap*****");
+temp=num2;
+num2=num1;
+num1=num3;
+num3=temp;
+console.log(num1,num2,num3);
+
