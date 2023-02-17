7e18c20a416756ab8ba2e3ca94756e248b972352
aishusankpal0511 <aishusankpal0511@gmail.com>
2023-02-17 10:17:57 +0530

    udated data types
    udated data types

---
+++ 02datatypes.js
@@ -0,0 +1,32 @@
+var myName = 'Virat Kohli';
+var fatherName = "Yogesh Kohli"
+
+var age = 31;
+var weight = 82.5;
+
+var isMarried = true;
+var isLazy = false; 
+
+var typeOfMyName  = typeof myName;
+console.log("Typeof variable myName: ",  typeOfMyName);
+console.log("typeOf variable fatherName:", typeof fatherName)
+
+console.log("Typeof age is: ",  age);
+console.log("Typeof weight is: ",  weight);
+
+console.log("Typeof isMarried is: ",  isMarried);
+console.log("Typeof isLazy is: ",  isLazy);
+
+var graduation;
+console.log("Value of  graduation variable is: ", graduation);
+console.log("typeOf graduation is: ", graduation);
+
+var city = null;
+console.log("Value of  city variable is: ", city);
+console.log("typeOf city is: ", typeof city);
+
+age = "32";
+console.log(typeof age);
+
+var isMarried = true;
+console.log(typeof  isMarried);
