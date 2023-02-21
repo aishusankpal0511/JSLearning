7de5451f162c20ab4b1f1bec76b41480d8fd93e0
aishusankpal0511 <aishusankpal0511@gmail.com>
2023-02-21 16:32:55 +0530

    updated string assign 01
    updated string assign 01

---
+++ 01stringTemplateAssign.js
@@ -0,0 +1,27 @@
+console.log("**********step1**********");
+//function with no arguments and no return value
+function showMyDreamCompany(){
+    console.log("My dream comapny is: Infosys")
+}
+showMyDreamCompany(); //function call or invoke
+console.log("********step2********")
+var myHobby1 = "Reading books";
+var myHobby2 = "Travelling";
+var myHobby3 = "watching motivational videos";
+console.log("My Hobbies are:",myHobby1,myHobby2,myHobby3);
+ console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+ var lengthHobby1 = myHobby1.length;
+ var lengthHobby2 = myHobby2.length;
+ var lengthHobby3 = myHobby3.length;
+ console.log(lengthHobby1, lengthHobby2, lengthHobby3);
+
+ function charOfHobby(myHobby1,myHobby2,myHobby3){
+    console.log("Total Number of character available in hobbies are:",myHobby1,myHobby2,myHobby3);
+    var sumOfChar = myHobby1 + myHobby2 + myHobby3;
+    console.log("sum of total number of characters that available in hobbies:",sumOfChar);
+}
+charOfHobby(13,10,28)
+
+
+
+ 
