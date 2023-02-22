9937a97399ef9ae577efe95e7c914489cb101bd2
aishusankpal0511 <aishusankpal0511@gmail.com>
2023-02-22 12:38:18 +0530

    updated operator
    updated operator

---
+++ 05 operator.js
@@ -0,0 +1,49 @@
+var num1 = 10;
+var num2 = 2;
+var result = num1 + num2; // Addition
+console.log(` Addition is ${result}`);
+
+result = num1 - num2;
+console.log(`Subtraction is  ${result}`);
+
+result = num1 * num2;
+console.log(`Multiplication is  ${result}`);
+
+ result = num1 ** num2;
+ console.log(`Exponential is  ${result}`);
+
+ result = num1 / 3;
+ console.log(`Division is  ${result}`);
+
+ result = num1 % 3;
+ console.log(`Reminder is  ${result}`);
+
+ var num = 10; // Compound addition +=
+ num+=20; // num = num + 20
+ console.log(`Compound addition += ${num}`);
+
+ console.log('caparison operators');
+ var num3 = 10;
+ console.log(num3==10);
+
+ console.log(`Caparison operators `);
+ var num3 = "10";
+ var num4 = 10;
+ console.log(num3==num4); // 10==10
+ console.log(num3===num4);
+
+ console.log(num3 != num4); //! not
+
+ console.log(10 > 20); // > Greater than
+
+ console.log(20 < 10); // > less than
+
+ console.log(10 < 10); // > less than
+
+ var marks = 35;
+ console.log(marks > 35); // > Greater than
+
+ var marks = 35;
+ console.log(marks >= 35); // > Greater than equal
+ 
+
