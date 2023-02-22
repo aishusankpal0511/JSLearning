a7e9b3faad7989f03f24cdf93be6fead43eb95cb
aishusankpal0511 <aishusankpal0511@gmail.com>
2023-02-22 12:40:09 +0530

    updated string
    updated string

---
+++ 04 stringTemplate.js
@@ -0,0 +1,11 @@
+
+
+
+
+console.log('you are "champ" man');
+var firstName = "Mohit";
+var lastName = "Sharma";
+var city = "Pune";
+console.log("My first name",firstName, "last name",lastName, "city",city);
+
+console.log(`My first name ${firstName} last name${lastName} city ${city}`);
