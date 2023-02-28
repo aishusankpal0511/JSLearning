1e09f0b728a98399610e708879b44da9bafe1b18
aishusankpal0511 <aishusankpal0511@gmail.com>
2023-02-28 11:23:56 +0530

    updated switchCase assign01
    updated switchCase assign01

---
+++ 01switchCaseAssign.js
@@ -0,0 +1,69 @@
+function monthDay(month){
+    switch (month) {
+        case 1:
+            console.log(`The 1st Month is:January`);
+         break;
+         case 2 :
+            console.log(`The 2nd Month is:February`);
+         break;
+         case 3 :
+            console.log(`The 3rd Month is:March`);
+         break;
+         case 4 :
+            console.log(`The 4th Month is:April`);
+         break;
+         case 5 :
+            console.log(`The 5th Month is:May`);
+         break;
+         case 6 :
+            console.log(`The 6th Month is:June`);
+         break;
+         case 7 :
+            console.log(`The 7th Month is:July`);
+         break;
+         case 8 :
+            console.log(`The 8th Month is:August`);
+         break;
+         case 9 :
+            console.log(`The 9th Month is:Setember`);
+         break;
+         case 10 :
+            console.log(`The 10th Month is:october`);
+         break;
+         case 11 :
+            console.log(`The 11th Month is:November`);
+         break;
+         case 12 :
+            console.log(`The 12th Month is:December`);
+         break;
+         default:
+         console.log(`Invalid month : ${month}`);
+         break;
+        }
+        
+       }
+       monthDay(2);
+       monthDay(5);
+       monthDay(12);
+       monthDay(15);
+       monthDay(100);
+       monthDay(null);
+       monthDay(undefined);
+       
+
+       
+       
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
