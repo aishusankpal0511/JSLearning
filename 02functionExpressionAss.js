da088137a5f32dc1c372e8c9790396a370de5ba4
aishusankpal0511 <aishusankpal0511@gmail.com>
2023-02-28 11:24:45 +0530

    updated function expression assign
    updated function expression assign

---
+++ 02functionExpressionAss.js
@@ -0,0 +1,27 @@
+function CheckLeap (leapYear){
+    if (typeof leapYear != 'number' || leapYear== undefined||leapYear==null||isNaN(leapYear)) {
+        console.log(`${leapYear} is not a leap year,please provide valid data`);
+
+        
+    }
+     else if (leapYear % 100 == 0 && leapYear % 400==0 || leapYear %100!=0 && leapYear%4==0) {
+        console.log(`${leapYear} is leap year`);
+     } else {
+        console.log(`${leapYear} is not leap year`);
+        
+        
+    }
+}
+CheckLeap (2020);
+CheckLeap (1999);
+CheckLeap (1600);
+CheckLeap (2022);
+CheckLeap (1945);
+CheckLeap (null);
+CheckLeap ("Twenty Twenty");
+CheckLeap (undefined);
+CheckLeap (NaN);
+CheckLeap (1750);
+
+
+
