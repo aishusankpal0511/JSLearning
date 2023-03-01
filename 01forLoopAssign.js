f87c30525e8509b1c957cdbc256d07e96abdbe62
aishusankpal0511 <aishusankpal0511@gmail.com>
2023-03-01 10:43:55 +0530

    for Loop Assign 01

---
+++ 01forLoopAssign.js
@@ -0,0 +1,37 @@
+console.log("print numbers from 5 to 15 by incrementing 1");
+for (let index = 5; index <=15; index=index+1) {
+   console.log(index);
+    
+}
+console.log("print numbers from 50 to 40 by decrementing by 1");
+for (let index = 50; index >=40; index= index-1) {
+   console.log(index);
+   
+}
+console.log("find first 15 odd number");
+for (let index = 1; index <=30; index=index+2) {
+   console.log(index);
+   
+}
+console.log("find first 10 even number");
+for (let index = 0; index <=20; index=index+2) {
+   console.log(index);
+   
+}
+console.log("print table of 5 like 5 to 50");
+for (let index = 5; index <=50; index=index+5) {
+  console.log(index);
+   
+}
+console.log("print table of 10 like 10 to 100");
+for (let index = 10; index <=100; index=index+10) {
+   console.log(index);
+   
+}
+console.log("print table of 10 in reverse order 100 to 10");
+for (let index = 100; index >=10; index=index-10) {
+   console.log(index);
+   
+}
+
+    
