2de37d3dceb6bd16812a21e7be22efca90fb69ae
aishusankpal0511 <aishusankpal0511@gmail.com>
2023-03-01 10:44:30 +0530

    for loop

---
+++ 06 for loops.js
@@ -0,0 +1,29 @@
+//print numbers 1 to 10
+//initialization 1
+//update Expression +1
+//condition <=10
+for (let index = 1; index <=10; index=index+1) { 
+    console.log(index);
+    
+}
+
+//print 0 to 5  // initialization 0    condition <=5 index +1
+for (let index = 0; index <=5; index = index+1) {
+    console.log(index);
+    
+}
+console.log("Reverse number");
+//print 10 to 0
+for (let index = 10; index >=0; index = index-1) {
+    console.log(index);
+    
+}
+ console.log("Table of 5")
+ for (let index = 5; index <=50; index= index+5) {
+    console.log(index); // 5 10 50
+}  
+console.log("**********"); 
+// for (let index = 1; index <10; index++) {
+//     console.log(index);
+// }
+ 
