f96d2078530add39248e4ab181c83978d5ecd08a
aishusankpal0511 <aishusankpal0511@gmail.com>
2023-03-01 10:45:16 +0530

    increment decrement program

---
+++ 05incrementDecrement.js
@@ -0,0 +1,9 @@
+// index++ ==> index = index + 1
+// index -- ==> index = index - 1
+index = 10;
+var y = index + 1
+console.log(`index: ${index}, y: ${y}`);
+
+index = 10;
+var y = ++index
+console.log(`index: ${index}, y: ${y}`);
