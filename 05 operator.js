0b1c70d47d4431bcd5d3d7c7e87a26d90d759bad
aishusankpal0511 <aishusankpal0511@gmail.com>
2023-03-20 19:03:27 +0530

    operator
    operator

--- 05 operator.js
+++ 05 operator.js
@@ -32,18 +32,64 @@ console.log(`Multiplication is  ${result}`);
  console.log(num3==num4); // 10==10
  console.log(num3===num4);
 
- console.log(num3 != num4); //! not
-
- console.log(10 > 20); // > Greater than
-
+ console.log(num3 != num4); 
  console.log(20 < 10); // > less than
 
  console.log(10 < 10); // > less than
 
  var marks = 35;
+
  console.log(marks > 35); // > Greater than
 
  var marks = 35;
  console.log(marks >= 35); // > Greater than equal
- 
 
+ var marks = 70;
+ var result = marks >=60 ? "Allow him for interview" :"Dont allow"; 
+ console.log(result);
+
+ var age = 20;
+ var res = age >= 21 ? true : false;
+ console.log(res);
+
+console.log("Even or OOD");
+ var myNumber = 7;
+ // Even - Divide by 2 and reminder is 0
+ var result = myNumber%2 == 0 ? "EVEN" : "OOD";
+ console.log(result);
+
+ function wordLength(word){
+    var wordLength = word.length;
+    var result = wordLength%2==0 ? "EVEN" : "ODD";
+    return result;
+ }
+ var result = wordLength("JavaScript");
+ console.log(`Word "JavaScript" has ${result} length` );
+ // var result = wordLength("Google");
+ console.log(`Word "Google" has ${wordLength("Google Chrome")} length` );
+
+ function maleMarriageEligibility(gender, age, boyName){
+    var result = (gender=="Male" && age>=21) ? `Hey ${boyName} you are eligible for marriage` : " No Try next time" ;
+    return result;
+ }
+  var result = maleMarriageEligibility("Male", 25, "Billgates");
+  console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
