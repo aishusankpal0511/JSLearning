c26d46f3f82aac655081c91ccb8f10d8186627b7
aishusankpal0511 <aishusankpal0511@gmail.com>
2023-03-20 19:01:33 +0530

    class
    class

--- 10.class.js
+++ 10.class.js
@@ -6,10 +6,37 @@ this.account = account;
 this.ifsc = ifsc;
 this.interestRate=interestRate;
     }
+    showDetails(){
+        console.log(`Bank Details: ${this.bankName} ${this.location} ${this.account} ${this.ifsc} ${this.interestRate}`);
+    }
 }
 const sbiBank = new Bank("SBI Bank","Narhe","62547895546","SBIN00004598",10.75);
-console.log(sbiBank);
+sbiBank.showDetails();
 
 const axisBank = new Bank("Axis Bank","wakad","895456211","AXIS789456", "10");
-console.log(axisBank);
+axisBank.showDetails();
+function traverseOject(bankObject){
+    for (const key in bankObject) {
+        if (Object.hasOwnProperty.call(bankObject, key)) {
+            const element = bankObject[key];
+            console.log(`Object key value is: ${key} ${element}`);
+            
+        }
+    }
 
+}
+traverseOject(sbiBank)
+
+
+class Person {
+    constructor(fullName, city){
+        this.fullName = fullName;
+        this.city = city;
+    }
+}
+const personDhoni = new Person("MS Dhoni", "Ranchi");
+const personShubham = new Person("Shubham Gill", "Mumbai");
+
+console.log(personDhoni instanceof Person);
+console.log(personShubham instanceof Bank);
+console.log(sbiBank instanceof Bank);
