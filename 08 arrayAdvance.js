781021d875c5015bd8a4e9c0a9f8b3146972ad25
aishusankpal0511 <aishusankpal0511@gmail.com>
2023-03-20 19:00:39 +0530

    arrayAdvance
    arrayAdvance

---
+++ 08 arrayAdvance.js
@@ -0,0 +1,15 @@
+console.log("===============join array=====================");
+const arrayOfNames =["Anil","Sunil","Sita","Bill","Elon"];
+const result = arrayOfNames.join(",");
+console.log(result);
+console.log(typeof result);
+
+console.log("==============Resixe an array==================");
+console.log(arrayOfNames.length);
+
+arrayOfNames.length = 3;
+console.log(arrayOfNames.length);
+console.log(arrayOfNames);
+
+arrayOfNames.length =7;
+console.log(arrayOfNames);
