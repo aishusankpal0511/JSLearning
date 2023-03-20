168fa7cd080da872bd19db6816ee98f2b374d9a0
aishusankpal0511 <aishusankpal0511@gmail.com>
2023-03-20 18:57:30 +0530

    cloning
    cloning

--- 08arrayAssign02.js
+++ 08arrayAssign02.js
@@ -1,4 +1,4 @@
-const arrayNumbers = [20,31,40,25,23,11,29,9,60,2,11];
+ const arrayNumbers = [20,31,40,25,23,11,29,9,60,2,11];
 console.log("======total elements available==========");
 const totalArrayElement = arrayNumbers.length;
 console.log(totalArrayElement);
---
+++ 10 cloning.js
@@ -0,0 +1,11 @@
+const person = {
+    fullName: " MS Dhoni",
+    age : 38
+}
+Object.freeze(person);
+
+let s1 = "Hey JS";
+let s2 = s1;
+
+let b1 = true;
+let b2 = b1;
